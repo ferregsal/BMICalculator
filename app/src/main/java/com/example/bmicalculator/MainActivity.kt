25e555f6bfe9df6c1165e39e22c6d3b6e59d184a
fregidorsalazar <fregidorsalazar@gmail.com>
2024-06-04 14:00:28 +0200

    Empty project

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.bmicalculator
 
+
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
+
+    lateinit var welcomeTextView: TextView
+    lateinit var clickButton: Button
+    var veces = 0
+    var colorRandom = Random.nextInt(-8252080, 8252080)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        welcomeTextView = findViewById(R.id.welcomeTextView)
+        welcomeTextView.text ="Bienvenido a mi app"
+
+        clickButton = findViewById(R.id.clickButton)
+        clickButton.setOnClickListener {
+            welcomeTextView.text = "El botón ha sido pulsado ${veces++} veces ${getRandomColor()}"
+            it.setBackgroundColor(getRandomColor())
+        }
     }
+        private fun getRandomColor(): Int {
+            val red = Random.nextInt(256)
+            val green = Random.nextInt(256)
+            val blue = Random.nextInt(256)
+            return Color.rgb(red, green, blue)
+        }
+
 }
