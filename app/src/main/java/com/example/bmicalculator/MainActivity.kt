9b7419d4d4beb50641328623cf22b181f2a1895d
fregidorsalazar <fregidorsalazar@gmail.com>
2024-06-07 13:52:19 +0200

    Empty project

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -11,10 +11,16 @@ import kotlin.math.pow
 import android.graphics.Color
 import android.util.Log
 import android.widget.SeekBar
+import com.google.android.material.slider.Slider
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.MotionEvent
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var heightEditText: EditText
+    //lateinit var heightEditText: EditText
     lateinit var weightTextView: TextView
     lateinit var resultTextView: TextView
     lateinit var resultCalculate: TextView
@@ -23,15 +29,21 @@ class MainActivity : AppCompatActivity() {
     lateinit var addButton: Button
     lateinit var checkLevel:TextView
     lateinit var seekBar: SeekBar
-
+    lateinit var heightTextView: TextView
+    lateinit var heightSlider : Slider
     var height:Int=150
     var weight:Int=70
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var decrementRunnable: Runnable
+    private lateinit var incrementRunnable: Runnable
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        heightEditText = findViewById(R.id.heightEditText)
+       // heightEditText = findViewById(R.id.heightEditText)
+        heightTextView = findViewById(R.id.heightTextView)
         minusButton = findViewById(R.id.minusButton)
         addButton = findViewById(R.id.addButton)
         weightTextView = findViewById(R.id.weightTextView)
@@ -42,22 +54,64 @@ class MainActivity : AppCompatActivity() {
         calculateButton = findViewById(R.id.calculateButton)
         checkLevel = findViewById(R.id.checkLevel)
         seekBar =findViewById(R.id.seekBar)
+        heightSlider =findViewById(R.id.heightSlider)
+        heightSlider.value=height.toFloat()
         setHeight()
         setWeight()
 
-        minusButton.setOnClickListener{
-            weight--
-            setWeight()
+        heightSlider.addOnChangeListener { _,value,_->
+
+            height=value.toInt()
+            setHeight()
         }
-        addButton.setOnClickListener{
+
+
+        // Incrementar el peso
+        addButton.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    handler.post(incrementRunnable)
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    handler.removeCallbacks(incrementRunnable)
+                }
+            }
+            true
+        }
+
+        // Decrementar el peso
+        minusButton.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    handler.post(decrementRunnable)
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    handler.removeCallbacks(decrementRunnable)
+                }
+            }
+            true
+        }
+
+        decrementRunnable = Runnable {
+            if (weight > 0) {
+                weight--
+                setWeight()
+                seekBar.progress = weight
+                handler.postDelayed(decrementRunnable, 100) // 100 ms delay
+            }
+        }
+
+        incrementRunnable = Runnable {
             weight++
             setWeight()
+            seekBar.progress = weight
+            handler.postDelayed(incrementRunnable, 100) // 100 ms delay
         }
 
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 weight = progress
-                weightTextView.text = progress.toString()
+                weightTextView.text = progress.toString()+" Kg"
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -70,39 +124,46 @@ class MainActivity : AppCompatActivity() {
         })
 
         calculateButton.setOnClickListener{
-            height=heightEditText.text.toString().toInt()
+            height=height.toString().toInt()
             val result = weight / (height/100f).pow(2)
-            resultCalculate.text = result.toString()
+            resultCalculate.text = "%.2f".format(result)
             when (result){
                 in (1f..18.4f) -> {
                     checkLevel.text="Insuficiente"
                     checkLevel.setTextColor(Color.BLUE)
+                    resultCalculate.setTextColor(Color.BLUE)
+
 
                 }
                 in (18.5f..24.9f) -> {
                     checkLevel.text="Normal"
                     checkLevel.setTextColor(Color.GREEN)
+                    resultCalculate.setTextColor(Color.GREEN)
                 }
                 in (25f..29f) -> {
                     checkLevel.text="Pre-obesidad"
                     checkLevel.setTextColor(Color.YELLOW)
+                    resultCalculate.setTextColor(Color.YELLOW)
                 }
                 in (30f..50f) -> {
                     checkLevel.text="Obesidad"
                     checkLevel.setTextColor(Color.RED)
+                    resultCalculate.setTextColor(Color.RED)
                 }
-                in (50f..100f) -> {
+                else -> {
                     checkLevel.text="Peligro!!"
-                    checkLevel.setTextColor(Color.BLACK)
+                    checkLevel.setTextColor(Color.MAGENTA)
+                    resultCalculate.setTextColor(Color.MAGENTA)
                 }
             }
         }
     }
     fun setHeight(){
-        heightEditText.setText(height.toString())
+        //heightEditText.setText(height.toString())
+        heightTextView.setText(height.toString()+" cm")
     }
     fun setWeight(){
-        weightTextView.text = weight.toString()
+        weightTextView.text = weight.toString()+" Kg"
     }
 
 
